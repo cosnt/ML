77e049253ea16f80f2f153ead522c8e03ac415cd
liuyu <liu.yu@datatom.com>
2017-11-27 18:12:31 +0800

    pca

--- unsupervised_learning/principal_component_analysis.py
+++ unsupervised_learning/principal_component_analysis.py
@@ -1,8 +1,18 @@
 import numpy as np
 from utils.operation import normalization
 
-def pca(data, k=2, tolerance = 0.9):
+def pca(data, k=2, tolerance = None):
     data = normalization(data,'Standardization')
-    m = len(data)
-    sigma = (1/m)*(data.T*data)
-    U, Sigma, V = np.linalg.svd(sigma)
+    sigma = data.T*data
+    U, lambdas, V = np.linalg.svd(sigma)
+    if tolerance is not None:
+        lambda_sum = sum(lambdas)
+        for index in range(len(lambdas)):
+            if sum(lambdas[:index+1])/lambda_sum:
+                U = U[:,:index+1]
+    else:
+        U = U[:,:k]
+    return data.T*U, U
+
+def kernel_pca(data, k=2, tolerance=None):
+    pass
